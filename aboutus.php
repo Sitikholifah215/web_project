64053a00c16314b8a5c04069126cc89f23115fcb
Sitikholifah215 <skholifah215@gmail.com>
2017-02-25 13:46:24 +0700

    upload pertamaku

---
+++ aboutus.php
@@ -0,0 +1,100 @@
+<html>
+	<head>
+		<title>
+			ABOUT US
+		</title>
+		<link rel="icon" type="image/png" href="../tugasakhir/logoo.png">
+	</head>
+	<head> 
+	<style>
+	table, th, td {
+    border: 1px  ;
+    border-collapse: collapse;
+	text : vijaya}
+	</style>
+	</head>
+	<body bgcolor="#ac7339">
+		<table width="80%" align="center" bgcolor="white">
+			<tr>
+				<td>
+					<table width="100%" bgcolor="white">
+						<tr>
+						<th height="250px"><img src="../tugasakhir/labell.jpg" width="100%" height="100%" title="Ole_ole.com"></th>
+						</tr>
+						<tr>
+						<th  bgcolor="white" height="10px"></th>
+						</tr>
+					</table>
+					
+					<table width="100%" background="pink1.png" align="center">
+						<tr height="40px">
+							<th width="50%">
+								<select>
+								
+									<option><pre>Cari...........................................................................................................................<br><br></pre></option>
+									<option>Batik</option>
+									<option>Food khas</option>
+									<option>Miniatur</option>
+									<option>Cake</option>
+								</select>
+							</th> 
+							<th width="5%"><img src="../tugasakhir/pencarian.png" width="30px" height="30px" align="center" title="Search"></th>
+							<th width="35%">
+								<table width="100%"bgcolor="white" align="center">
+									<tr height="25px">
+										<td width="100%"><font color ="BROWN" size="3" face="Lucida Calligraphy" align="left"> <?php echo $_GET["nama"]; ?> </td>
+									</tr>
+									<tr height="25px">
+										<td width="100%"><font color ="BROWN" size="3" face="Lucida Calligraphy"> <?php echo $_GET["email"]; ?> </td>
+									</tr>	
+								</table>
+							</th> 
+							<th width="8%"><img src="../tugasakhir/orang.png" width="50px" height="50px" align="center" title="login"></th></th>
+						</tr>
+					</table>
+					<table width="100%" bgcolor="#86592d" align="center">
+						<tr height="55px">
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="home.html">HOME</th> <th width="1%"></th>
+							<th><font color ="yellow" size="3" face="Lucida Handwriting"><a href="aboutus.html">ABOUT US</a></th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="produk.html">PRODUCT</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="newsand.html">NEWS & PROMOTIONS</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="testimoni.html">TESTIMONI</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="location.html">LOCATION</th>
+						</tr>
+						<tr height="10px">
+							<th></th>
+							<th></th>
+						</tr>
+					</table>
+					<table>
+					<tr height="300px">
+							<th>isi about</th>
+						</tr>
+					</table>
+					<table width="100%" bgcolor="white" align="center">
+						<tr height="120px">
+							<th ><img src="../tugasakhir/logoo.png" width="180px" height="180px" title="Ole_ole.com"></th>
+							<th >
+								<pre><font color ="black" align="left" size="3px" face="Tahoma"> PT.OLEH-OLEH INA Tbk. <br>  Jl.Ifa Kholifah no 001 Jakarta Selatan  <br> Tlep. 021-31631631,31131131 <br>  Fax. 021-31231231  </pre>
+							</th>
+							<th ><img src="../tugasakhir/app.png" width="270px%" height="90px" title="App play store"><br><br><br><br></th>
+							<th >
+								<table width="100%"bgcolor="white" align="center">
+									<tr height="60px">
+										<th width="100%"><img src="../tugasakhir/r.png" width="370px" height="90px" title="PT. OLEH-OLEH INA Tbk."><br><br></th>
+									</tr>
+									<tr height="60px">
+										<th width="100%"><pre>                           <img src="../tugasakhir/bahasa.png" width="170px%" height="50px" title="language"></pre></th>
+									</tr>	
+								</table>
+							</th>
+						</tr>
+						<tr height="10px">
+							<th></th>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
---
+++ tugasakhirrr.php
@@ -0,0 +1,237 @@
+<html>
+	<head>
+		<title>
+			Ole_ole.com
+		</title>
+		<link rel="icon" type="image/png" href="../tugasakhir/logoo.png">
+	</head>
+	<head>
+	<style>
+	table, th, td {
+    border: 1px  ;
+    border-collapse: collapse;
+	text : vijaya}
+	</style>
+	<style>
+.dropbtn {
+    background-color: white;
+    color: white;
+    padding: 11px;
+    font-size: 10px;
+    border: none;
+    cursor: pointer;
+}
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: pink;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+.dropdown:hover .dropbtn {
+    background-color: white;
+}
+</style>
+	</head>
+	<body bgcolor="#ac7339">
+		<table width="80%" align="center" bgcolor="white">
+			<tr>
+				<td>
+					<table width="100%" bgcolor="white">
+						<tr>
+						<th height="250px"><img src="../tugasakhir/labell.jpg" width="100%" height="100%" title="Ole_ole.com"></th>
+						</tr>
+						<tr>
+						<th  bgcolor="white" height="10px"></th>
+						</tr>
+					</table>
+					
+					<table width="100%" background="pink1.png" align="center">
+						<tr height="40px">
+							<th width="50%">
+								<div class="dropdown">
+									<button class="dropbtn" bgcolor="pink"><font color ="BROWN" size="3" " align="left">Search..............................................................................................................
+									<img src="../tugasakhir/pencarian.png" width="30px" height="30px" align="center" title="Search"></button>
+									<div class="dropdown-content">
+										<a href="batik.html">Batik		..............................................................................................................</a>
+										<a href="food.html">Food		..............................................................................................................</a>
+										<a href="miniatur.html">Miniatur	.......................................................................................................</a>
+										<a href="cake.html">Cake		..............................................................................................................</a>
+									</div>
+								</div>
+							</th> 
+						
+							<th width="35%">
+								<table width="100%"bgcolor="white" align="center">
+									<tr height="25px">
+										<td width="100%"><font color ="BROWN" size="3" face="Lucida Calligraphy" align="left"> <?php echo $_GET["nama"]; ?> </td>
+									</tr>
+									<tr height="25px">
+										<td width="100%"><font color ="BROWN" size="3" face="Lucida Calligraphy"> <?php echo $_GET["email"]; ?> </td>
+									</tr>	
+								</table>
+							</th> 
+							<th width="8%">
+							<div class="dropdown">
+									<button class="dropbtn" bgcolor="pink"><font color ="BROWN" size="3" " >
+									<img src="../tugasakhir/orang.png" width="50px" height="50px" align="center" title="login"></button>
+									<div class="dropdown-content">
+										<a href="login.html">Log Out</a>
+									</div>
+								</div>
+							</th>
+						</tr>
+					</table>
+					<table width="100%" bgcolor="pink" align="center">
+						<tr height="55px">
+							<th><font color ="yellow" size="3" face="Lucida Handwriting"><a href="home.html">HOME</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="aboutus.html">ABOUT US</a></th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="produk.html">PRODUCT</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="newsand.html">NEWS & PROMOTIONS</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="testimoni.html">TESTIMONI</th> <th width="1%"></th>
+							<th><font color ="white" size="3" face="Lucida Handwriting"><a href="location.html">LOCATION</th>
+						</tr>
+						<tr height="10px">
+							<th></th>
+							<th></th>
+						</tr>
+					</table>
+					<table width="100%" bgcolor="white">
+					<tr height="10px">
+							<th></th>
+						</tr>
+						<tr height="60px">
+							<th><pre><font color ="black" align="left" size="6px" face="algerian">PICTURE PRODUCT'S SLIDESHOW<pre></th>
+						</tr>
+						
+					</table>
+					
+					<table width="100%" bgcolor="white" align="center">
+						<tr height="300px">
+							<th width="15%" align="center"><a href="home.html"><img src="../tugasakhir/kiri.gif-c200" width="100px" height="90px" title="left"></th>
+							
+							<th width="70%" align="left">
+								<div class="w3-content w3-section" style="max-width:500px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/1.jpg" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/12.jpg" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-bottom" src="../tugasakhir/slide/11.jpg" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/7.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/5.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-bottom" src="../tugasakhir/slide/6.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/4.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/3.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-bottom" src="../tugasakhir/slide/2.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/8.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-right" src="../tugasakhir/slide/9.png" width="815px" height="425px">
+									<a href="beli.html"><img class="mySlides w3-animate-bottom" src="../tugasakhir/slide/10.jpg" width="815px" height="425px">
+								</div>
+								<script>
+								var myIndex = 0;
+								carousel();
+
+								function carousel() {
+									var i;
+									var x = document.getElementsByClassName("mySlides");
+    								for (i = 0; i < x.length; i++) {
+ 								     x[i].style.display = "none";  
+    								}
+    								myIndex++;
+   								 if (myIndex > x.length) {myIndex = 1}    
+    								x[myIndex-1].style.display = "block";  
+    								setTimeout(carousel, 2500);   
+									
+								}
+								</script>
+							</th>
+							<th width="15%" align="center"><a href="home.html"><img src="../tugasakhir/kanan.gif-c200" width="100px" height="90px" title="right"></th>
+							</tr>
+					</table>
+					<table>
+						<tr height="40px">
+							<th><p style="font:bold 30px Gabriola ;color:blue" align=center><marquee> PUSAT OLEH-OLEH INDONESIA PUSAT OLEH-OLEH INDONESIA PUSAT OLEH-OLEH INDONESIA PUSAT OLEH-OLEH INDONESIA PUSAT OLEH-OLEH INDONESIA </marquee></p></th>
+						</tr>
+						<tr height="10px">
+							<th></th>
+						</tr>
+					</table>
+					
+					<table width="100%" bgcolor="white" align="center">
+					
+						<tr height="55px">
+							<th><pre><font color ="black" align="left" size="6px" face="algerian">HOT PRODUCT</th>
+						</tr>
+						<tr>
+						<th  bgcolor="white" height="10px"></th>
+						</tr>
+					</table>
+					<table width="100%" bgcolor="white" align="center">
+						<tr height="200px">
+							<th width="25%"><a href="beli.html"><img src="../tugasakhir/BATIKGEDOG.png" width="100%" height="270px"  title="batik gedog"></th>
+							<th width="25%"><a href="beli.html"><img src="../tugasakhir/cake.png" width="100%"  height="270px"  title="cake"></th>
+							<th width="25%"><a href="beli.html"><img src="../tugasakhir/rendang.png" width="100%" height="270px" title="rendang indonesia"></th>
+							<th width="25%"><a href="beli.html"><img src="../tugasakhir/miatur.png" width="100%" height="270px"  title="miniatur"></th>
+							
+						</tr>
+						<tr height="150px">
+							<th width="25%">BATIK is a techque of wax-resist dyeing applied to ehole cloth, or cloth made using this technique. Batk is made either by drawing dots and lines of the resist with a spouted tool called <br> a canting, by printing the resist with a copper stamp called a cap. .......... </th>
+							<th width="25%">A classic cake with tangy touch our moist Apple forest is made with freshly chopped apples and a hint of spices. <br> A luscious, Smooth whipped cream coversthe cake and chocolate mousse separates the layers. Simply delicious. .......... </th>
+							<th width="25%">RENDANG is a spicy meat dish which originated from the Minangkabau ethnic group of Indonesia and is now commonly served across the country. One of the characteristic foods of Minangkabau culture, it is served at ceremonial occasions and to honour guests. ..........</th>
+							<th width="25%">Miniatur is a replica of an object such as a place, building, food, and other objects that can be seen from any direction or so-called three-dimensional object. Miniature usually made for an art exhibition or event that requires a demonstration. ..........</th>
+							
+						</tr>
+					</table>
+					<table width="100%" bgcolor="white" align="center">
+						<tr>
+							<th height="160px"><a href="allproduk.html"><img src="../tugasakhir/lead more.png" width="115px" height="110px" align="center" title="more"></th>
+						</tr>
+						<tr>
+							<th><a href="#TOP"><br> Back to top </a></th>
+						</tr>
+					</table>
+					<table width="100%" bgcolor="white" align="center">
+						<tr height="120px">
+							<th ><img src="../tugasakhir/logoo.png" width="180px" height="180px" title="Ole_ole.com"></th>
+							<th >
+								<pre><font color ="black" align="left" size="3px" face="Tahoma"> PT.OLEH-OLEH INA Tbk. <br>  Jl.Ifa Kholifah no 001 Jakarta Selatan  <br> Tlep. 021-31631631,31131131 <br>  Fax. 021-31231231  </pre>
+							</th>
+							<th ><a href="downloadapp.html"><img src="../tugasakhir/app.png" width="270px%" height="90px" title="App play store"><br><br><br><br></th>
+							<th >
+								<table width="100%"bgcolor="white" align="center">
+									<tr height="60px">
+										<th width="100%"><img src="../tugasakhir/r.png" width="370px" height="90px" title="PT. OLEH-OLEH INA Tbk."><br><br></th>
+									</tr>
+									<tr height="60px">
+										<th width="100%"><pre>                           </pre></th>
+									</tr>	
+								</table>
+							</th>
+						</tr>
+						<tr height="10px">
+							<th></th>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
